4ef0d298a7f48b36b59fbf2ce947f70ac5776981
Linoel Yang <pzydy1990@gmail.com>
2015-05-06 17:38:54 +0800

    add new code

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,14 +1,5 @@
-var postsData = [{
-    title: 'Introducint Telescope',
-    url: 'http://sachagreif.com/introducint-telescope/'
-}, {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-}, {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-}];
-
 Template.postsList.helpers({
-    posts: postsData
+    posts: function(){
+    	return Posts.find();
+    }
 });
